4685ec4f2eb18623e9f00353b543c8bf3505fca8
CeliaLInaresA <celialinares21@gmail.com>
2015-05-19 19:30:22 -0500

    add js_session01 files

---
+++ js_session01/script.js
@@ -0,0 +1,64 @@
+//print all variables, utilizar solamente en casos extraordinarios (links de referencia, léxico especializado etc)
+var isVerified = true;
+console.log(isVerified);
+
+var address = "N. García #172";
+console.log(address);
+
+var name;
+console.log(name);
+
+var nullValue = null;
+console.log(nullValue);
+
+var edad1 = 18;
+console.log(num1);
+
+var edad2 = 17;
+console.log(num2);
+
+var estatura = 1.20;
+console.log(estatura);
+
+//using operators
+console.log(isVerified !)
+
+console.log(edad1 == edad2);
+
+console.log(edad2 < edad1);
+console.log(edad2  edad1);
+console.log(edad1<=edad2);
+console.log(edad1 = edad2);
+console.log
+
+//typeof exercise
+var numberr = 10;
+var booleann = true;
+var stringg = "palabra";
+var nulll= null;
+var unknownn;
+
+console.log(typeof nulll); //object
+console.log(typeof numberr); //number
+console.log(typeof booleann); //boolean
+console.log(typeof stringg); //string
+console.log(typeof unknownn); //undefined
+
+//antro exercise
+var age = prompt("your age?");
+var gender = "female";
+var isMom = false;
+var legalAgeFemale = (age = 18 && gender = "female");
+
+if (isMom){
+    alert ("gratis por el 10 de mayo);
+}
+else if (legalAgeFemale){
+    alert ("entra gratis!!");
+} 
+else{
+    alert ("paga");
+}
+
+
+
